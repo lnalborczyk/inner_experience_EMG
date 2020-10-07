6506196bfcd719bd5b09516a9bd9b70e4edf24aa
lnalborczyk <ladislas.nalborczyk@gmail.com>
2020-10-07 21:33:37 +0200

    minor updates in readme

--- code/bf_simulation.R
+++ code/bf_simulation.R
@@ -3,7 +3,7 @@
 # Simulating the effect of sample size on BFs          #
 # ---------------------------------------------------- #
 # Ladislas Nalborczyk                                  #
-# Last updated on 05.10.2020                           #
+# Last updated on 08.10.2020                           #
 # https://github.com/lnalborczyk/inner_experience_EMG  #
 ########################################################
 
@@ -123,34 +123,3 @@ for (i in 1:nrow(overall_results) ) {
 
 # saving the results
 # save(overall_results, file = "results/overall_results.Rda")
-
-###############################
-# Speeding up the simulations #
-###############################
-
-# https://datashenanigan.wordpress.com/2016/01/15/speeding-bayesian-power-analysis-t-test-up-with-snowfall/
-# https://www4.stat.ncsu.edu/~post/isaac/parallel_programming_R.pdf
-
-# vectorisation using sapply (slightly faster, ~1.48mn for 1 sim, against 1.69mn for the for loop)
-# set.seed(123)
-# start_time <- proc.time()
-# sapply(sample_size, simulating_bfs)
-# print(proc.time() - start_time)
-
-# parallelisation using the parallel package
-# library(parallel)
-# num_cores <- detectCores() # / 2
-# my_cluster <- makeCluster(num_cores)
-# start_time <- proc.time()
-# results_parallel <- parSapply(my_cluster, sample_size, simulating_bfs)
-# print(proc.time() - start_time)
-# shut down the cluster
-# stopCluster(my_cluster)
-
-# parallelisation using the doParallel package (slightly faster, around 1.75mn for 1 simulation)
-# library(doParallel)
-# cl <- makeCluster(4)
-# registerDoParallel(cl)
-# start_time <- proc.time()
-# results_foreach <- foreach(n = sample_size, .packages = c("brms", "BayesFactor", "tidyverse") ) %dopar% simulating_bfs(n)
-# print(proc.time() - start_time)
